7482d49abeee55bd33bcc991875d88e0f6c7ce96
ArduLite <ardulite@gmail.com>
2025-03-09 15:53:42 +0700

    Update Analog_Read_Control_Led_Brightness.ino

--- examples/Analog_Read_Control_Led_Brightness/Analog_Read_Control_Led_Brightness.ino
+++ examples/Analog_Read_Control_Led_Brightness/Analog_Read_Control_Led_Brightness.ino
@@ -2,18 +2,13 @@
 
 int main(){
   PWM led(9);
+  Analog sensor(A0);
 
   while(1){
-    
-    for(byte i = 0; i < 255; i++){
-      led.write(i);
-      wait(5);
-    }
 
-    for(byte i = 255; i > 0; i--){
-      led.write(i);
-      wait(5);
-    }
+    int sensorValue = sensor.read();
+    float brightness = sensorValue * (255.0 / 1023.0);
+    led.write(brightness);
     
   }
 }
